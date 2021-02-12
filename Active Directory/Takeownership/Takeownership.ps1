41ecaee6d179862983646437e69fa91ac7fd68cb
Dylan <dylangrove@outlook.com>
2021-02-11 23:29:46 -0700

    Test

--- Active Directory/Takeownership/Takeownership.ps1
+++ Active Directory/Takeownership/Takeownership.ps1
@@ -2,3 +2,5 @@
 
 get-childitem $folder -Recurse -Hidden | %{ takeown /f $_.FullName  /a }
 get-childitem $folder -Recurse -Hidden | %{ icacls $_.FullName /grant administrators:F /inheritance:r}
+
+#test  
