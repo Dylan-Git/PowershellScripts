04f40981882eb6dc90a92a6ee13e60744975dfc1
Dylan <dylangrove@outlook.com>
2021-02-11 23:32:49 -0700

    test3

--- Active Directory/Takeownership/Takeownership.ps1
+++ Active Directory/Takeownership/Takeownership.ps1
@@ -1,4 +1,4 @@
 ﻿$folder = Read-Host -Prompt "Folder path"
-#test
+#test2
 get-childitem $folder -Recurse -Hidden | %{ takeown /f $_.FullName  /a }
 get-childitem $folder -Recurse -Hidden | %{ icacls $_.FullName /grant administrators:F /inheritance:r}
